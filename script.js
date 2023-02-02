78048311cf49513ec581ec1d92e75485392d4377
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-02-02 22:40:38 +0000

    level 100

--- script.js
+++ script.js
@@ -4,9 +4,34 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function getSeasonAndEpisodeFormat(episode) {
+  const { season, number } = episode;
+  const paddedSeason = season.toString().padStart(2, "0");
+  const paddedEpisode = number.toString().padStart(2, "0");
+
+  return `S${paddedSeason}E${paddedEpisode}`;
+}
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach((episode) => {
+    // add episode's name and the season
+    const episodeName = document.createElement("h5");
+    episodeName.innerText = `${getSeasonAndEpisodeFormat(episode)}: ${
+      episode.name
+    }`;
+    rootElem.appendChild(episodeName);
+
+    // add the image
+    const image = document.createElement("img");
+    image.src = episode.image.medium;
+    rootElem.appendChild(image);
+
+    //add the paragraph
+    rootElem.innerHTML += episode.summary;
+  });
 }
 
 window.onload = setup;
